a4ead8dc33bdbf627d0f2458a2fb06fe34d79b8f
Jeremie Dimino <jdimino@janestreet.com>
2017-02-01 16:35:29 +0000

    114.30+61

--- common/std_types.ml
+++ common/std_types.ml
@@ -1,4 +1,4 @@
-open Core_kernel.Std
+open Core_kernel
 
 module Package_checksum = String
 module Package_name     = String
--- common/std_types.mli
+++ common/std_types.mli
@@ -1,4 +1,4 @@
-open Core_kernel.Std
+open Core_kernel
 
 module Package_checksum : Identifiable.S
 module Package_name     : Identifiable.S
